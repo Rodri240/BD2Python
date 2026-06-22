960e4fcd32a9d643c3d5aaebf9c603c1df96d182
GamarraDiego <diegolgamarra@gmail.com>
2026-06-22 13:17:03 -0300

    add requirements and environment variables

--- TicketWorldCup/Database/connection.py
+++ TicketWorldCup/Database/connection.py
@@ -1,10 +1,11 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="mysql.reto-ucu.net",     
-        port = 50006, 
-        user=" xr_g6_admin",
-        password="OBLBD2ST",
-        database="XR_Grupo6"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
